8de6ab54a027b1c119b2f2f0eae38039ba138ccf
marta.prieto <marta.prieto@ac247202r.onmicrosoft.com>
2023-08-01 12:29:44 +0200

    Add css

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
 
 
 function getRandom(max) {
-                
-    let result = Math.floor(Math.random() * (max-1+1)) + 1;
+
+    let result = Math.floor(Math.random() * (max - 1 + 1)) + 1;
     let btn = document.querySelector(".random_button");
     btn.innerHTML = result;
 }
 
-
 function clickButton() {
-    let button100 = document.querySelector(".button1");
-    let button6 = document.querySelector(".button2");
-    let button3 = document.querySelector(".button3");
-
     //array para recorrer los elementos 
-    /*let arrayBtn = document.querySelectorAll(".button_dice");
+    let arrayBtn = document.querySelectorAll(".button_dice");
 
-    arrayBtn.forEach(button => {
-        console.log("prueba", button)
-
-        button.addEventListener("click", () => {
-            console.log("prueba2")
-        })
-    });*/
-
-
-    if (button100) {
-        
-        button100.addEventListener("click", () => {
-            getRandom(100);
+    arrayBtn.forEach(btn => {
+        btn.addEventListener("click", () => {
+                getRandom(Number(btn.getAttribute("nr_max")));
+                //console.log("cuál es el botón clicado", btn)
+       
         });
-    }
-    
-    if (button6) {
-        button6.addEventListener("click", () => {
-            getRandom(6);
-        });
-    }
-    if (button3) {
-        
-        button3.addEventListener("click", () => {
-            getRandom(3);
-        });
-    }
+    });
 
 }
 
