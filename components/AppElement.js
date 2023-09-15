8660217d7a4c4b50e506f9c4fb61711b3640c932
marta.prieto <marta.prieto@ac247202r.onmicrosoft.com>
2023-09-15 17:42:56 +0200

    Add class

--- components/AppElement.js
+++ components/AppElement.js
@@ -1,19 +1,13 @@
-function getRandom(max) {
-
-    let result = Math.floor(Math.random() * (max - 1 + 1)) + 1;
-    let btn = document.querySelector(".random_button");
-    btn.innerHTML = result;
-}
+import { getRandom } from "./class.js"
 
 function clickButton() {
-    //array para recorrer los elementos 
-    let arrayBtn = document.querySelectorAll(".button_dice");
+    const arrayBtn = document.querySelectorAll(".button_dice")
 
     console.error("arra", arrayBtn)
     arrayBtn.forEach(btn => {
         btn.addEventListener("click", () => {
-                getRandom(Number(btn.getAttribute("nr_max")));
-        });
+                getRandom(Number(btn.getAttribute("nr_max")))
+        })
     })
 }
-clickButton();
+clickButton()
--- components/class.js
+++ components/class.js
@@ -1,6 +1,21 @@
-class Rectangulo {
+/*class Rectangulo {
     constructor(alto, ancho) {
       this.alto = alto;
       this.ancho = ancho;
     }
+  }*/
+
+class Dices {
+  getRandom(){
+    let result = Math.floor(Math.random() * (max - 1 + 1)) + 1
+    let btn = document.querySelector(".random_button")
+    btn.innerHTML = result;
+    console.log("prueba")
   }
+}
+
+const dice = new Dices()
+dice.getRandom()
+
+
+
